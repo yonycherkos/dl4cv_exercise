ab29fd622b05b1453fa8eab97a1a6c2e348308c3
yonycherkos <yonycherkos@gmail.com>
2020-10-04 17:05:02 +0300

    7.1.3 A Basic Image Preprocessor

---
+++ pyimagesearch/preprocessing/simplePreprocessor.py
@@ -0,0 +1,11 @@
+import cv2
+
+class SimplePreprocessor():
+    def __init__(self, height, width, inter=cv2.INTER_AREA):
+        super().__init__()
+        self.height = height
+        self.width = width
+        self.inter = inter
+
+    def preprocess(self, image):
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
